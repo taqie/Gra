78bd062131d8940d5a7ba9c8042a8a2966c58034
Taqie <graca.kubicki@gmail.com>
2016-06-09 00:07:14 +0200

    init

---
+++ android/src/pl/taq/pierwsza/graasdasd/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package pl.taq.pierwsza.graasdasd;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import pl.taq.pierwsza.graasdasd.asdasd;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new asdasd(), config);
+	}
+}
---
+++ desktop/src/pl/taq/pierwsza/graasdasd/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.taq.pierwsza.graasdasd.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.taq.pierwsza.graasdasd.asdasd;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new asdasd(), config);
+	}
+}
