09460597d16d16958bf018293eb710ec0a4fceb5
taqie <graca.kubicki@gmail.com>
2016-06-14 18:36:00 +0200

    add player button and click lisner

--- core/src/pl/taq/pierwsza/screen/GamePlayScreeen.java
+++ core/src/pl/taq/pierwsza/screen/GamePlayScreeen.java
@@ -1,5 +1,9 @@
 package pl.taq.pierwsza.screen;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
 import pl.taq.pierwsza.entitis.Player;
 import pl.taq.pierwsza.graasdasd.asdasd;
 
@@ -9,7 +13,7 @@ import pl.taq.pierwsza.graasdasd.asdasd;
 public class GamePlayScreeen extends AbstractScreen {
 
     private Player player;
-
+    private Button playerButton;
     public GamePlayScreeen(asdasd game) {
         super(game);
 
@@ -17,13 +21,31 @@ public class GamePlayScreeen extends AbstractScreen {
     @Override
     protected void init(){
         initPlayer();
+        initPlayerButton();
     }
     private void initPlayer(){
         player = new Player();
         stage.addActor(player);
 
     }
+    private  void initPlayerButton(){
+        playerButton = new Button(new Button.ButtonStyle());
+        playerButton.setWidth(460);
+        playerButton.setHeight(360);
+        playerButton.setY(170);
+        playerButton.setX(10);
+        playerButton.setDebug(true);
 
+        stage.addActor(playerButton);
+        playerButton.addListener(new ClickListener(){
+
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+
+                return super.touchDown(event, x, y, pointer, button);
+            }
+        });
+    }
     @Override
     public void render(float delta) {
         super.render(delta);
